0dcecce8321a027d24dc4ce7acbd07c82f04f9e0
Miller <vcmiller@wpi.edu>
2018-09-14 20:02:19 -0400

    asdf

---
+++ Assets/Materials/SnailMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnailMovement : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float velocity;
+    public float acceleration;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rb.velocity = Vector3.MoveTowards(rb.velocity, v * velocity, acceleration * Time.deltaTime);
+	}
+}
