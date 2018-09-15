96859170eb163977480c7240b1de1be10d960d93
Miller <vcmiller@wpi.edu>
2018-09-15 01:26:18 -0400

    asdf

--- Assets/Materials/SnailMovement.cs
+++ Assets/Materials/SnailMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SnailMovement : MonoBehaviour {
@@ -8,6 +9,11 @@ public class SnailMovement : MonoBehaviour {
     public float velocity;
 
     public SnailSection[] bodySections;
+    public float connectionStrength = 200;
+
+    public int headSections = 2;
+
+    public Transform[] eyeStalks;
 
 	// Use this for initialization
 	void Start () {
@@ -26,34 +32,61 @@ public class SnailMovement : MonoBehaviour {
         
         ss.rb.AddForce(v * velocity, ForceMode.Acceleration);
 
+        var firstStuck = bodySections.FirstOrDefault(s => s.isStuck);
 
-        if (Input.GetButton("Jump")) {
-            ss.sticky = bodySections[0].sticky = false;
-            ss.rb.AddForce(Vector3.up * 50);
+        bool up = Input.GetButton("Jump") && firstStuck;
+        if (up) {
+            for (int i = 0; i < headSections; i++) {
+                bodySections[i].sticky = false;
+                bodySections[i].useGravity = false;
+                //bodySections[i].rb.AddForce(v * velocity, ForceMode.Acceleration);
+            }
+            ss.sticky = false;
+            ss.useGravity = false;
+            ss.rb.AddForce(firstStuck.gravityNormal * 40);
         } else {
-            ss.sticky = bodySections[0].sticky = true;
+            ss.sticky = true;
+            ss.useGravity = true;
+            for (int i = 0; i < headSections; i++) {
+                bodySections[i].sticky = true;
+                bodySections[i].useGravity = true;
+            }
         }
+
+        if (firstStuck) {
+            var u = firstStuck.gravityNormal;
+            ss.rb.AddTorque(Vector3.Cross(ss.transform.up, ss.isStuck ? ss.gravityNormal : Vector3.ProjectOnPlane(u, ss.transform.forward)) * 10);
+        }
+
+
         
         var lastSection = ss;
-        var lastPos = ss.rb.transform.position - ss.rb.transform.forward * 0.5f;
+        var lastPos = ss.rb.transform.position - ss.rb.transform.forward * ss.radius;
         var lastUp = ss.rb.transform.up;
 
-        foreach (var section in bodySections) {
-            var curPosOnSection = section.transform.position + section.transform.forward * 0.5f;
-            section.rb.AddForceAtPosition((lastPos - curPosOnSection) * 100, curPosOnSection);
-            lastSection.rb.AddForceAtPosition((curPosOnSection - lastPos) * 100, lastPos);
+        for (int i = 0; i < bodySections.Length; i++) {
+            var section = bodySections[i];
+            var curPosOnSection = section.transform.position + section.transform.forward * section.radius;
+            section.rb.AddForceAtPosition((lastPos - curPosOnSection) * connectionStrength, curPosOnSection);
+            lastSection.rb.AddForceAtPosition((curPosOnSection - lastPos) * connectionStrength, lastPos);
 
             var curUp = section.transform.up;
-            section.rb.AddTorque(Vector3.Cross(curUp, lastUp) * 10);
+
+            section.rb.AddTorque(Vector3.Cross(curUp, ss.isStuck ? ss.gravityNormal : Vector3.ProjectOnPlane(lastUp, section.transform.forward)) * 10);
 
             Debug.DrawLine(curPosOnSection, lastPos);
 
-            lastPos = section.transform.position - section.transform.forward * 0.5f;
+            lastPos = section.transform.position - section.transform.forward * section.radius;
             lastSection = section;
             lastUp = curUp;
 
+            if (section.isStuck) {
+                section.rb.velocity = Vector3.Project(section.rb.velocity, section.transform.forward);
+            }
+        }
 
-            section.rb.velocity = Vector3.Project(section.rb.velocity, section.transform.forward);
+        foreach (var stalk in eyeStalks) {
+            stalk.transform.right = -(firstStuck?.gravityNormal ?? Vector3.up);
         }
         
 	}
--- Assets/Scripts/SnailSection.cs
+++ Assets/Scripts/SnailSection.cs
@@ -7,6 +7,8 @@ public class SnailSection : MonoBehaviour {
     public float stickyStrength = 50;
 
     public float gravityStrength = 20;
+    public bool useGravity = true;
+    public float radius;
 
     public Rigidbody rb { get; private set; }
     private Material mat;
@@ -15,7 +17,15 @@ public class SnailSection : MonoBehaviour {
     public bool isStuck => sticky && grounded;
     public Vector3 gravityNormal { get; private set; }
 
+    public SnailSection head;
+
     public bool sticky = true;
+
+
+    public Transform meshNode;
+
+    public Vector3 rotationToApply;
+    private Quaternion quatToApply;
 
     private float drag;
 
@@ -24,26 +34,35 @@ public class SnailSection : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         mat = GetComponent<MeshRenderer>().material;
         drag = rb.drag;
-	}
+        quatToApply = Quaternion.Euler(rotationToApply);
+    }
 	
 	// Update is called once per frame
 	void Update () {
         RaycastHit hit;
         grounded = false;
         gravityNormal = Vector3.zero;
-        if (Physics.Raycast(new Ray(transform.position, -transform.up), out hit, raycastDistance + 0.45f, 1)) {
-            if (sticky) {
+        if (Physics.Raycast(new Ray(transform.position, -transform.up), out hit, raycastDistance + radius - 0.05f, 1)) {
+            bool sticc = sticky && (head == this || !head.isStuck || head.gravityNormal == hit.normal);
+            if (sticc) {
                 gravityNormal = hit.normal;
-                rb.AddForceAtPosition(gravityNormal * -stickyStrength, transform.position - transform.up * 0.5f, ForceMode.Acceleration);
+                rb.AddForceAtPosition(gravityNormal * -stickyStrength, transform.position - transform.up * radius, ForceMode.Acceleration);
             }
             grounded = true;
         }
 
         //rb.drag = rb.angularDrag = isStuck ? drag : 0;
-        if (!isStuck) {
-            rb.AddForceAtPosition(Vector3.down * gravityStrength, transform.position - transform.up * 0.5f, ForceMode.Acceleration);
+        if (!isStuck && useGravity) {
+            rb.AddForceAtPosition(Vector3.down * gravityStrength, transform.position - transform.up * radius, ForceMode.Acceleration);
         }
 
         mat.color = grounded ? Color.white : Color.red;
 	}
+
+    private void LateUpdate() {
+        if (meshNode) {
+            meshNode.transform.position = transform.position;
+            meshNode.transform.rotation = transform.rotation * quatToApply;
+        }
+    }
 }
