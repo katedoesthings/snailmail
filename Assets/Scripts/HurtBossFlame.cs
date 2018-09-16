b52d3809699f92d0e1bb79a891c44d1dfae0050d
Miller <vcmiller@wpi.edu>
2018-09-16 15:39:53 -0400

    asdf

---
+++ Assets/Scripts/HurtBossFlame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtBossFlame : MonoBehaviour {
+    public float burnTime;
+
+    private float remaining;
+    private ParticleSystem.EmissionModule em;
+
+	// Use this for initialization
+	void Start () {
+        em = GetComponent<ParticleSystem>().emission;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (remaining > 0) {
+            remaining -= Time.deltaTime;
+            if (remaining <= 0) {
+                em.enabled = false;
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider collision) {
+        var flame = collision.transform.root.GetComponentInChildren<FlamePowerup>();
+        if (flame.active) {
+            em.enabled = true;
+            remaining = burnTime;
+        }
+    }
+}
