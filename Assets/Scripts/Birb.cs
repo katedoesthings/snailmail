e1f5cfcbde9f1ea45d0326361cc2e4b358426751
Miller <vcmiller@wpi.edu>
2018-09-16 12:54:39 -0400

    asdf

--- Assets/Scripts/Birb.cs
+++ Assets/Scripts/Birb.cs
@@ -30,10 +30,10 @@ public class Birb : MonoBehaviour {
         float progress = Time.time % cycleTime;
 
         if (progress < cycleTime * 0.25f) {
-            transform.position = Vector3.Lerp(v1, v2, progress * 4.0f / 5.0f);
+            transform.position = Vector3.Lerp(v1, v2, progress * 4.0f / cycleTime);
             transform.forward = f1;
         } else if (progress < cycleTime * 0.5f) {
-            transform.position = Vector3.Lerp(v2, v3, progress * 4.0f / 5.0f - 1.0f);
+            transform.position = Vector3.Lerp(v2, v3, progress * 4.0f / cycleTime - 1.0f);
             transform.forward = f1;
             if (!caw.isPlaying)
             {
@@ -41,10 +41,10 @@ public class Birb : MonoBehaviour {
             }
             
         } else if (progress < cycleTime * 0.75f) {
-            transform.position = Vector3.Lerp(v3, v2, progress * 4.0f / 5.0f - 2.0f);
+            transform.position = Vector3.Lerp(v3, v2, progress * 4.0f / cycleTime - 2.0f);
             transform.forward = f2;
         } else {
-            transform.position = Vector3.Lerp(v2, v1, progress * 4.0f / 5.0f - 3.0f);
+            transform.position = Vector3.Lerp(v2, v1, progress * 4.0f / cycleTime - 3.0f);
             transform.forward = f2;
         }
 	}
