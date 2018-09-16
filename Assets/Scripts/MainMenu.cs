019a9e27d9ba1e8d014e1b4f43987ffe89b6bd8a
Miller <vcmiller@wpi.edu>
2018-09-16 12:24:06 -0400

    asdf

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public string gameLevel;
+
+    public void Play() {
+        SceneManager.LoadScene(gameLevel);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
